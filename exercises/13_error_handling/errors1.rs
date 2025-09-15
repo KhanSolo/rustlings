201ea9362a9e8c4004af99f404bc6bba88f3349e
Konstantin <drpdrp@mail.ru>
2025-09-15 22:00:41 +0300

    threads2

--- exercises/13_error_handling/errors1.rs
+++ exercises/13_error_handling/errors1.rs
@@ -1,5 +1,3 @@
-use std::fmt::Error;
-
 // TODO: This function refuses to generate text to be printed on a nametag if
 // you pass it an empty string. It'd be nicer if it explained what the problem
 // was instead of just returning `None`. Thankfully, Rust has a similar
